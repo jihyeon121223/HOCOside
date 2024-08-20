4f25ab040e5d61b04023c0163611d49848f3b4f3
yujihyeon <maintain@todaygate-tech.com>
2024-08-20 22:23:55 +0900

    second commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-const MyConst : string = '<div class=""></div>'
+// const MyConst: string = '<div class="my-class">Hello, Haco!</div>'; //TypeScript
+const MyConst = '<div class="my-class">Hello, World!</div>'; //JavaScript
+
+function addElementToBody() {
+    const body = document.querySelector('body');
+    if (body) {
+        body.innerHTML += MyConst;
+        }
+}
+
+window.onload = addElementToBody;
