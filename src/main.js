f0e8e788f2fe81d413031a36f2d0116eb335a1b5
yujihyeon <maintain@todaygate-tech.com>
2024-08-21 00:18:41 +0900

    fifth commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // const MyConst: string = '<div class="my-class">Hello, Haco!</div>'; //TypeScript
-const MyConst = '<div class="my-class">Hello, World!</div>'; //JavaScript
+// const MyConst = '<div class="my-class">Hello, Haco!</div>'; //JavaScript
 
 function extractWords() {
 
@@ -7,23 +7,33 @@ function extractWords() {
     const textInput = document.getElementById('textInput').value;
 
     // 전처리
-    const words = textInput.match(/\b\w+\b/g);
+    const words = textInput.toLowerCase().match(/\b\w+\b/g);
 
-    // 결과값
-    const wordsList = document.getElementById('wordsList');
-    wordsList.innerHTML = '';
-
-    //반복문 
+    // 반복문 (도움받음..)
     const frequency= {};
     
     if(words){
-        words.forEach((word) => {
-            const wordElement = document.createElement('li');
-            wordElement.textContent = word;
-            wordsList.appendChild(wordElement);
-            });
+        words.forEach(word => {
+            frequency[word] = (frequency[word] || 0) + 1;
+        });
+
+        // 빈도수
+        const sortedWords = Object.entries(frequency)
+        .sort((a, b) => b[1] - a[1]);
+
+        // 결과값 
+        const wordsList = document.getElementById('wordsList');
+        wordsList.innerHTML = '';
+
+        sortedWords.forEach(([word, count]) => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${word}: (${count})`;
+            wordsList.appendChild(listItem);
+        });
+
     }else{
-        wordsList.innerHTML = '<li>반복 단어가 없습니다.</li>'
+        const wordsList = document.getElementById('wordsList');
+        wordsList.innerHTML = '<li>반복 단어가 없습니다.</li>';
     }
 }
 
