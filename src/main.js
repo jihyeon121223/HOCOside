af6014d61493ed7a6097ef34368c1e44c46e3b12
yujihyeon <maintain@todaygate-tech.com>
2024-08-20 23:21:22 +0900

    third commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 // const MyConst: string = '<div class="my-class">Hello, Haco!</div>'; //TypeScript
 const MyConst = '<div class="my-class">Hello, World!</div>'; //JavaScript
 
-function addElementToBody() {
-    const body = document.querySelector('body');
-    if (body) {
-        body.innerHTML += MyConst;
-        }
+function extractWords() {
+
+    // 인풋값
+    const textInput = document.getElementById('textInput').value;
+
+    // 전처리
+    const words = textInput.match(/\b\w+\b/g);
+
+    // 결과값
+    const wordsList = document.getElementById('wordsList');
+    wordsList.innerHTML = '';
+
+    //반복문 
+    const frequency= {};
+    
+    if(words){
+        words.forEach((word) => {
+            const wordElement = document.createElement('li');
+            wordElement.textContent = word;
+            wordsList.appendChild(wordElement);
+            });
+    }else{
+        wordsList.innerHTML = '<li>반복 단어가 없습니다.</li>'
+    }
 }
+
 
 window.onload = addElementToBody;
